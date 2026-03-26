962baccc97993696170e29d222965773ec875550
PrajwolKhadka <prazolkhadka67@gmail.com>
2026-03-26 18:30:35 +0545

    feat(keys)\

---
+++ src/app/components/Key.tsx
@@ -0,0 +1,24 @@
+type keyProps = {
+    note: any;
+    isBlack? : boolean;
+    active?: boolean;
+    onStart: ()=> void;
+    onStop: () => void;
+};
+
+export default function key({note, isBlack, active, onStart, onStop}: keyProps){
+    return(
+        <button
+        onMouseDown={onStart}
+        onMouseUp={onStop}
+        onMouseLeave={onStop}
+        className={`${isBlack?
+             `w-8 h-28 bg-black text-white z-10 ${active? "bg-orange-300": "bg-black"}`
+             : `w-14 h-48  text-black border flex flex-col justify-end items-center pb-2 ${active? "bg-orange-300": "bg-white"}`
+        }`}
+        >
+            <span>{note.note}</span>
+            <span className="text-xs">{note.key.toUpperCase()}</span>
+        </button>
+    );
+}
---
+++ src/app/components/Keyboard.tsx
@@ -0,0 +1,82 @@
+import { NOTES } from "../utils/notes";
+import Key from "./Key";
+
+type Props = {
+  activeKeys: Set<string>;
+  onStart: (key: string, freq: number) => void;
+  onStop: (key: string) => void;
+  getFrequency: (freq: number) => number;
+};
+
+export default function Keyboard({
+  activeKeys,
+  onStart,
+  onStop,
+  getFrequency,
+}: Props) {
+  const whiteKeys = NOTES.filter((n) => n.type === "white");
+  const blackKeys = NOTES.filter((n) => n.type === "black");
+  const WHITE_KEY_WIDTH = 56;
+  const WHITE_ORDER = ["C", "D", "E", "F", "G", "A", "B"];
+  const BLACK_LEFT_NOTE: Record<string, string> = {
+    "C#": "C",
+    "D#": "D",
+    "F#": "F",
+    "G#": "G",
+    "A#": "A",
+  };
+  const getBlackKeyLeft = (note: (typeof NOTES)[0]) => {
+    const leftNote = BLACK_LEFT_NOTE[note.note];
+    const whiteIndex = whiteKeys.findIndex(
+      (w) => w.note === leftNote && w.octave === note.octave,
+    );
+    // Position at 62% across the left white key
+    return whiteIndex * (WHITE_KEY_WIDTH + 2) + WHITE_KEY_WIDTH * 0.62;
+  };
+  return (
+    <div
+      className="relative"
+      style={{ width: whiteKeys.length * (WHITE_KEY_WIDTH + 2) }}
+    >
+      {/* //seto */}
+      <div className="flex gap-0.5">
+        {whiteKeys.map((note) => {
+          const id = `${note.note}${note.octave}`;
+          return (
+            <Key
+              key={id}
+              note={note}
+              active={activeKeys.has(id)}
+              onStart={() => {
+                onStart(id, getFrequency(note.freq));
+              }}
+              onStop={() => {
+                onStop(id);
+              }}
+            />
+          );
+        })}
+      </div>
+      {/* //kalo */}
+      {blackKeys.map((note) => {
+        const id = `${note.note}${note.octave}`;
+        return (
+          <div
+            key={id}
+            className="absolute top-0 z-10"
+            style={{ left: getBlackKeyLeft(note) }}
+          >
+            <Key
+              key={id}
+              note={note}
+              isBlack
+              active={activeKeys.has(id)}
+              onStart={() => onStart(id, getFrequency(note.freq))}
+              onStop={() => onStop(id)}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
