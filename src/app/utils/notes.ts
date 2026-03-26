c5ea8b71b481d74b1486e792dba0c1aeb81e3a25
PrajwolKhadka <prazolkhadka67@gmail.com>
2026-03-26 15:33:02 +0545

    Initial mapping

---
+++ src/app/utils/notes.ts
@@ -0,0 +1,14 @@
+export const Notes = [
+    {key:"a", note: "C", freq: 261.63, type: "white"},
+    {key:"w", note: "C#", freq: 277.18, type: "black"},
+    {key:"s", note: "D", freq: 293.66, type: "white"},
+    {key:"e", note: "D#", freq: 311.13, type: "black"},
+    {key:"d", note: "E", freq: 329.63, type: "white"},
+    {key:"f", note: "F", freq: 349.23, type: "white"},
+    {key:"t", note: "F#", freq: 369.99, type: "black"},
+    {key:"g", note: "G", freq: 392.00, type: "white"},
+    {key:"y", note: "G#", freq: 415.3, type: "black"},
+    {key:"h", note: "A", freq: 440.0, type: "white"},
+    {key:"u", note: "A#", freq: 446.16, type: "black"},
+    {key:"j", note: "B", freq: 493.88, type: "white"},
+];
